acfa362ab51f3b9b3e85e3fb6c79f384f5a8e1dd
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2021-08-06 19:58:44 +0200

    fix css

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -31,7 +31,7 @@
 
 
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="all">
+    <link rel="stylesheet" href="/css/style.css" type="text/css" media="all">
 
 
     <script type="text/javascript">
