1a0c9dada016cf314d1fc0ca90ff14b7f2f715d5
Ahmed Mahfouz <ahmed98mahfouz@gmail.com>
2021-08-06 19:23:45 +0200

    fix error 500

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\DashoardController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('dashboard.index');
     }
 }
